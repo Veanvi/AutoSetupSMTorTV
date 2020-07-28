3bfc3525e91f1d2efd86b700b556378593aa0fde
Veanvi <Veanvi@mail.ru>
2020-07-28 21:23:00 +0300

    изменен AutoSetupSMTorTv для загрузки скриптов с удаленного сервера

--- AutoSetupSMTorTV.bat
+++ AutoSetupSMTorTV.bat
@@ -16,23 +16,30 @@ exit /B
 :W7
 :: Определение версии Powershell
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine" >nul
+:: Стирание ошибки, чтобы пользователь не закрыл окно увидев ошибку.
+cls
 if !errorlevel! equ 0 (
   for /f "tokens=2*" %%a in ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine" /v PowerShellVersion') do set "psVersion=%%~b"
   set psVersion=!psVersion:~0,3!
   :: Если установлена Powershell более ранней версии 5.1 начинается установка 5.1. Иначе запускается установка TorTV
   if !psVersion! neq 5.1 (
-      powershell -executionpolicy RemoteSigned -file "C:\Users\123\Desktop\SetupWmfOnWin7.ps1"
+        powershell -ExecutionPolicy RemoteSigned -Command " iex ((New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Veanvi/AutoSetupSMTorTV/master/SetupWmfOnWin7.ps1', '%TEMP%\SetupWmfOnWin7.ps1'))"
+        cls
+        powershell -ExecutionPolicy RemoteSigned -file "%TEMP%\SetupWmfOnWin7.ps1"
   ) else (
-    powershell -executionpolicy RemoteSigned -file "C:\Users\123\Desktop\SetupAndStartupTorTV.ps1"
+    powershell -NoProfile -InputFormat None -ExecutionPolicy RemoteSigned -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Veanvi/AutoSetupSMTorTV/master/SetupAndStartupTorTV.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\SetupAndStartupTorTV"
   )
 ) else (
-  powershell -executionpolicy RemoteSigned -file "C:\Users\123\Desktop\SetupWmfOnWin7.ps1"
+  :: В Windows 7 не получилось запустить скрипт на прямую из сети
+  powershell -ExecutionPolicy RemoteSigned -Command " iex ((New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Veanvi/AutoSetupSMTorTV/master/SetupWmfOnWin7.ps1', '%TEMP%\SetupWmfOnWin7.ps1'))"
+  cls
+  powershell -ExecutionPolicy RemoteSigned -file "%TEMP%\SetupWmfOnWin7.ps1"
 )
 exit /B
 
 :: Выполняется если скрипт запущен не в Windows 7
 :Other
-powershell -executionpolicy RemoteSigned -file "C:\Users\123\Desktop\SetupAndStartupTorTV.ps1"
+powershell -NoProfile -InputFormat None -ExecutionPolicy RemoteSigned -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Veanvi/AutoSetupSMTorTV/master/SetupAndStartupTorTV.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\SetupAndStartupTorTV"
 
 exit /B
  
